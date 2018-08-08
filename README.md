3cb8319df3b2376c49eb70c913972713c223fd25
Luis Guilherme <luisteixeiira@gmail.com>
2018-08-08 19:30:01 -0300

    Configuração MVC iniciada e README atualizado

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # WebDieta
-Aplicação para acompanhamento de Dieta online.
+
+## Requirements
+* Python 3
+* Package psycopg2
+* PostgreSQL
+
+## Getting Started
+
+### Install psycopg2
+```
+$ pip3 install psycopg2
+$ pip3 install psycopg2-binary
+```
+
+### Install PostgreSQL Ubuntu
+```
+$ sudo apt-get update
+$ sudo apt-get install postgresql postgresql-contrib
+```
+
+### Configure PostgreSQL Ubuntu
+`$ sudo -u postgres psql`
+```SQL
+CREATE USER user WITH PASSWORD 'password';
+CREATE DATABASE webdieta WITH OWNER user ENCODING 'utf-8';
+```
--- webdieta/settings.py
+++ webdieta/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dieta',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'webdieta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['%s/templates/' % (PROJECT_DIR)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'webdieta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webdieta',
+        'USER': 'infoway',
+        'PASSWORD': 'Infow@y321',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
