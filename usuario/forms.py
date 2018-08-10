38c49761af340869293a99cddd9c05c2357ebcbe
Luis Guilherme <luisteixeiira@gmail.com>
2018-08-10 15:04:29 -0300

    Cadastro de usuário com todas as informações

---
+++ usuario/forms.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class FormularioCadastro(forms.Form):
+    first_name = forms.CharField(label='Nome')
+    last_name = forms.CharField(label='Sobrenome')
+    username = forms.CharField(label='Usuário', min_length=4, max_length=150)
+    email = forms.EmailField(label='E-mail')
+    password1 = forms.CharField(label='Digite sua senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirme sua senha', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Nome de usuário já cadastrado")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("E-mail já cadastrado")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Senhas não são iguais")
+
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            first_name=self.cleaned_data['first_name'],
+            last_name=self.cleaned_data['last_name'],
+            email=self.cleaned_data['email'],
+            password=self.cleaned_data['password1']
+        )
+        return user
--- usuario/views.py
+++ usuario/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from .forms import FormularioCadastro
 
 def cadastrar(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = FormularioCadastro(request.POST)
 
         if form.is_valid():
             form.save()
@@ -14,7 +15,7 @@ def cadastrar(request):
             login(request, user)
             return(redirect('/dashboard/'))
     else:
-        form = UserCreationForm()
+        form = FormularioCadastro()
 
     contexto = {'form' : form}
     return(render(request, 'registration/cadastrar.html', contexto))
