7dfcb59980fbf49d0205f1e5ee147c27dc148fda
Luis Guilherme <luisteixeiira@gmail.com>
2018-08-10 15:03:45 -0300

    Tela de login melhorada

--- webdieta/urls.py
+++ webdieta/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
     path('', include('dieta.urls')),
     path('', include('usuario.urls')),
     path('', include('django.contrib.auth.urls')),
