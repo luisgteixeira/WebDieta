1e098c7bd5b90c97b7d88e303c27a194a13bcd0c
Luis Guilherme <luisteixeiira@gmail.com>
2018-08-11 00:07:17 -0300

    Tela de registro de peso criada

--- dieta/urls.py
+++ dieta/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard),
-    path('dieta/', views.dieta)
+    path('dieta/', views.dieta),
+    path('registrar-peso/', views.registrarPeso, name="registrarPeso"),
+    path('dashboard/', views.dashboard)
 ]
--- dieta/views.py
+++ dieta/views.py
@@ -1,21 +1,34 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
+from .fusioncharts import FusionCharts
 from django.http import HttpResponse
 from collections import OrderedDict
 from django.shortcuts import render
+from .forms import FormularioPeso
 from dieta.models import *
-from .fusioncharts import FusionCharts
 import datetime
 
 @login_required(login_url='/login/')
 def dieta(request):
     return(render(request, 'dieta/dieta.html'))
 
+@login_required(login_url='/login/')
+def registrarPeso(request):
+    if(request.method == 'POST'):
+        form = FormularioPeso(request.POST)
+        if(form.is_valid()):
+            form.save(request.user)
+            return(redirect('/dashboard/'))
+    else:
+        form = FormularioPeso()
+
+    return(render(request, 'dieta/registrar-peso.html', {'form': form}))
+
 
 @login_required(login_url='/login/')
 def dashboard(request):
     usuario = request.user
-    pesos = Peso.objects.filter(usuario_id=usuario.id)
+    pesos = Peso.objects.filter(usuario_id=usuario.id).order_by('data_pesagem')
     graf = grafico(pesos)
 
     return(render(request, 'dieta/dashboard.html', graf))
@@ -43,6 +56,6 @@ def grafico(pesos):
         dados["value"] = peso.peso
         grafico["data"].append(dados)
 
-    grafico = FusionCharts("line", "ex1" , "60%", "50%", "chart-1", "json", grafico)
+    grafico = FusionCharts("line", "ex1" , "60%", "55%", "chart-1", "json", grafico)
 
     return({'output' : grafico.render()})
